d0bb53256ac35feb8f79e1b0aaff2ef1a16bf74e
DESKTOP-GHUDGOK\65208 <spf>
2022-08-27 23:37:03 +0800

    编辑器测试

---
+++ Assets/Scripts/System/testtool.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class jqmTools 
+{
+    [MenuItem("JqmTools/init Scenes")]
+    static void CreateSphere()
+    {
+        Debug.Log("hello  CreateSphere");
+    }
+
+
+}
