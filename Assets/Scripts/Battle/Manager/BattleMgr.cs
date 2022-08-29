0a8121c7b8ba0184a5353a03661ec87a384e97db
DESKTOP-GHUDGOK\65208 <spf>
2022-08-29 11:45:40 +0800

    11

--- Assets/Scripts/Battle/Manager/BattleMgr.cs
+++ Assets/Scripts/Battle/Manager/BattleMgr.cs
@@ -7,20 +7,57 @@ public class BattleMgr : MonoBehaviour {
     //private AudioSvc audioSvc;
     //private StateMgr stateMgr;
 
-    private stateMgr statreMgr;
+    private StateMgr stateMgr;
 
     public EntityPlayer ebtitySelfPlayer;
     private MapMgr mapMgr;
     private Dictionary<string, EntityMonster> monsterDic = new Dictionary<string, EntityMonster>();
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void LoadMonsterByWaveID(int wave)
+    {
+        for (int i = 0; i < mapCfg.monsterLst.Count; i++)
+        {
+            MonsterData md = mapCfg.monsterLst[i];
+            if (md.mWave == wave)
+            {
+                GameObject m = resSvc.LoadPrefab(md.mCfg.resPath, true);
+                m.transform.localPosition = md.mBornPos;
+                m.transform.localEulerAngles = md.mBornRote;
+                m.transform.localScale = Vector3.one;
+
+                m.name = "m" + md.mWave + "_" + md.mIndex;
+
+                EntityMonster em = new EntityMonster
+                {
+                    battleMgr = this,
+                    stateMgr = stateMgr,
+                    skillMgr = skillMgr
+                };
+                //设置初始属性
+                em.md = md;
+                em.SetBattleProps(md.mCfg.bps);
+                em.Name = m.name;
+
+                MonsterController mc = m.GetComponent<MonsterController>();
+                mc.Init();
+                em.SetCtrl(mc);
+
+                m.SetActive(false);
+                monsterDic.Add(m.name, em);
+                if (md.mCfg.mType == MonsterType.Normal)
+                {
+                    GameRoot.Instance.dynamicWnd.AddHpItemInfo(m.name, mc.hpRoot, em.HP);
+                }
+                else if (md.mCfg.mType == MonsterType.Boss)
+                {
+                    BattleSys.Instance.playerCtrlWnd.SetBossHPBarState(true);
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
