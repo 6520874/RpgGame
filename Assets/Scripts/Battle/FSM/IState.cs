0c905b420ded49cda4f45c5d81967cf4076d3fe5
DESKTOP-GHUDGOK\65208 <spf>
2022-08-29 10:12:36 +0800

    战斗逻辑

---
+++ Assets/Scripts/Battle/FSM/IState.cs
@@ -0,0 +1,21 @@
+﻿public interface IState
+{
+    void Enter(EntityBase entity, params object[] args);
+
+    void Process(EntityBase entity, params object[] args);
+
+    void Exit(EntityBase entity, params object[] args);
+
+}
+  
+public enum AniState {
+   None,
+   Born,
+   Idle,
+   Move,
+   Attack,
+   Hit,
+   Die
+}
+
+
--- Assets/Scripts/Battle/Manager/BattleMgr.cs
+++ Assets/Scripts/Battle/Manager/BattleMgr.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleMgr : MonoBehaviour {
+    private ResSvc resSvc;
+    //private AudioSvc audioSvc;
+    //private StateMgr stateMgr;
+
+    private stateMgr statreMgr;
+
+    public EntityPlayer ebtitySelfPlayer;
+    private MapMgr mapMgr;
+    private Dictionary<string, EntityMonster> monsterDic = new Dictionary<string, EntityMonster>();
 
 
 	// Use this for initialization
