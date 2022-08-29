0382184fb629c9f420da5b651ebc25dc334c34a1
DESKTOP-GHUDGOK\65208 <spf>
2022-08-29 11:40:21 +0800

    11

--- Assets/Scripts/Battle/Manager/MapMgr.cs
+++ Assets/Scripts/Battle/Manager/MapMgr.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapMgr : MonoBehaviour {
+    private BattleMgr battleMgr;
+    public TriggerData[] triggerArr;
 
 
     public void Init(BattleMgr battle)
@@ -10,13 +12,21 @@ public class MapMgr : MonoBehaviour {
         battleMgr = battle;
 
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    public void TriggerMonsterBorn(TriggerData trigger, int waveIndex)
+    {
+        if (battleMgr != null)
+        {
+            BoxCollider co = trigger.gameObject.GetComponent<BoxCollider>();
+            co.isTrigger = false;
+
+            battleMgr.LoadMonsterByWaveID(waveIndex);
+            battleMgr.ActiveCurrentBatchMonsters();
+            battleMgr.triggerCheck = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
