5b83bbce631e0ac8b555fdafc021cedcabfbcb61
DESKTOP-GHUDGOK\65208 <spf>
2022-08-29 10:16:41 +0800

    戰鬥

---
+++ Assets/Scripts/Battle/FSM/StateAttack.cs
@@ -0,0 +1,19 @@
+﻿class StateAttack : IState
+{
+    public void Enter(EntityBase entity, params object[] args)
+    {
+        entity.currentAniState = AniState.Attack;
+        //entity.curSkillCfg = ResSvc.Instance.GetSkillCfg();
+
+    }
+
+    public void Exit(EntityBase entity, params object[] args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Process(EntityBase entity, params object[] args)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Battle/Manager/SkillMgr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillMgr : MonoBehaviour {
+    private Dictionary<AnimationState, IState> fsm = new Dictionary<AnimationState, IState>();
+
+
+    public void Init(BattleMgr battle)
+    {
+         
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
