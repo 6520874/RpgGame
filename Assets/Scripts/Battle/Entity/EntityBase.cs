e48054c54c5e0105a3a1b3e3d4c701a8669f37f9
DESKTOP-GHUDGOK\65208 <spf>
2022-09-03 02:09:48 +0800

    技能

--- Assets/Scripts/Battle/Entity/EntityBase.cs
+++ Assets/Scripts/Battle/Entity/EntityBase.cs
@@ -1,45 +1,58 @@
-﻿using System.Collections;
+﻿/****************************************************
+	文件：EntityBase.cs
+	作者：Plane
+	邮箱: 1785275942@qq.com
+	日期：2019/03/19 6:40   	
+	功能：逻辑实体基类
+*****************************************************/
+
 using System.Collections.Generic;
 using UnityEngine;
 
-//逻辑实体基类
-// A behaviour that is attached to a playable
-public abstract class EntityBase
-{
+public abstract class EntityBase {
     public AniState currentAniState = AniState.None;
 
     public BattleMgr battleMgr = null;
     public StateMgr stateMgr = null;
     public SkillMgr skillMgr = null;
     protected Controller controller = null;
+    private string name;
 
     public bool canControl = true;
     public bool canRlsSkill = true;
-    private string name;
+
+
     public EntityType entityType = EntityType.None;
+
     public EntityState entityState = EntityState.None;
-    public string Name
-    {
-        get
-        {
+    public string Name {
+        get {
             return name;
         }
 
-        set
-        {
+        set {
             name = value;
         }
     }
+
+    private BattleProps props;
+    public BattleProps Props {
+        get {
+            return props;
+        }
+
+        protected set {
+            props = value;
+        }
+    }
+
     private int hp;
-    public int HP
-    {
-        get
-        {
+    public int HP {
+        get {
             return hp;
         }
 
-        set
-        {
+        set {
             //通知UI层TODO
             PECommon.Log(Name + ": HPchange:" + hp + " to " + value);
             SetHPVal(hp, value);
@@ -47,263 +60,91 @@ public abstract class EntityBase
         }
     }
 
-    public Queue<int> comboQue = new Queue<int>();
+public Queue<int> comboQue = new Queue<int>();
     public int nextSkillID = 0;
-    public int skEndCB = -1;
+
     public SkillCfg curtSkillCfg;
+
     //技能位移的回调ID
     public List<int> skMoveCBLst = new List<int>();
     //技能伤害计算回调ID
     public List<int> skActionCBLst = new List<int>();
 
-    public BattleProps Props
-    {
-        get
-        {
-            return props;
-        }
+    public int skEndCB = -1;
 
-        protected set
-        {
-            props = value;
+
+    public void Born() {
+        stateMgr.ChangeStatus(this, AniState.Born, null);
+    }
+    public void Move() {
+        stateMgr.ChangeStatus(this, AniState.Move, null);
+    }
+    public void Idle() {
+        stateMgr.ChangeStatus(this, AniState.Idle, null);
+    }
+    public void Attack(int skillID) {
+        stateMgr.ChangeStatus(this, AniState.Attack, skillID);
+    }
+    public void Hit() {
+        stateMgr.ChangeStatus(this, AniState.Hit, null);
+    }
+    public void Die() {
+        stateMgr.ChangeStatus(this, AniState.Die, null);
+    }
+
+    public virtual void TickAILogic() { }
+
+    public void SetCtrl(Controller ctrl) {
+        controller = ctrl;
+    }
+    public void SetActive(bool active = true) {
+        if (controller != null) {
+            controller.gameObject.SetActive(active);
         }
     }
-    public virtual void SetHPVal(int oldval, int newval)
-    {
-        if (controller != null)
-        {
-            //GameRoot.Instance.dynamicWnd.SetHPVal(Name, oldval, newval);
-        }
-    }
-    public virtual void SetBattleProps(BattleProps props)
-    {
+    public virtual void SetBattleProps(BattleProps props) {
         HP = props.hp;
         Props = props;
     }
 
-
-    public virtual void SetSkillMoveState(bool move, float speed = 0f)
-    {
-        if (controller != null)
-        {
+    public virtual void SetBlend(float blend) {
+        if (controller != null) {
+            controller.SetBlend(blend);
+        }
+    }
+    public virtual void SetDir(Vector2 dir) {
+        if (controller != null) {
+            controller.Dir = dir;
+        }
+    }
+    public virtual void SetAction(int act) {
+        if (controller != null) {
+            controller.SetAction(act);
+        }
+    }
+    public virtual void SetFX(string name, float destroy) {
+        if (controller != null) {
+            controller.SetFX(name, destroy);
+        }
+}
+    public virtual void SetSkillMoveState(bool move, float speed = 0f) {
+        if (controller != null) {
             controller.SetSkillMoveState(move, speed);
         }
     }
-    public virtual void SetAtkRotation(Vector2 dir, bool offset = false)
-    {
-        if (controller != null)
-        {
-            if (offset)
-            {
+    public virtual void SetAtkRotation(Vector2 dir, bool offset = false) {
+        if (controller != null) {
+            if (offset) {
                 controller.SetAtkRotationCam(dir);
             }
-            else
-            {
+            else {
                 controller.SetAtkRotationLocal(dir);
             }
         }
     }
 
-
-    public void RmvMoveCB(int tid)
-    {
-        int index = -1;
-        for (int i = 0; i < skMoveCBLst.Count; i++)
-        {
-            if (skMoveCBLst[i] == tid)
-            {
-                index = i;
-                break;
-            }
-        }
-        if (index != -1)
-        {
-            skMoveCBLst.RemoveAt(index);
-        }
-    }
-
-
-    public void RmvSkillCB()
-    {
-        SetDir(Vector2.zero);
-        SetSkillMoveState(false);
-
-        for (int i = 0; i < skMoveCBLst.Count; i++)
-        {
-            int tid = skMoveCBLst[i];
-            TimerSvc.Instance.DelTask(tid);
-        }
-
-        for (int i = 0; i < skActionCBLst.Count; i++)
-        {
-            int tid = skActionCBLst[i];
-            TimerSvc.Instance.DelTask(tid);
-        }
-
-        //攻击被中断，删除定时回调
-        if (skEndCB != -1)
-        {
-            TimerSvc.Instance.DelTask(skEndCB);
-            skEndCB = -1;
-        }
-        skMoveCBLst.Clear();
-        skActionCBLst.Clear();
-
-
-        //清空连招
-        if (nextSkillID != 0 || comboQue.Count > 0)
-        {
-            nextSkillID = 0;
-            comboQue.Clear();
-            battleMgr.lastAtkTime = 0;
-            battleMgr.comboIndex = 0;
-        }
-    }
-
-
-
-    public virtual void SetBlend(float blend)
-    {
-        if (controller != null)
-        {
-            controller.SetBlend(blend);
-        }
-
-    }
-
-    private BattleProps props;
-    public virtual void SetFX(string name, float destroy)
-    {
-        if (controller != null)
-        {
-            controller.SetFX(name, destroy);
-        }
-    }
-    public virtual void SetAction(int act)
-    {
-        if (controller != null)
-        {
-            controller.SetAction(act);
-        }
-    }
-    public void ExitCurtSkill()
-    {
-        canControl = true;
-
-        if (curtSkillCfg != null)
-        {
-            if (!curtSkillCfg.isBreak)
-            {
-                entityState = EntityState.None;
-            }
-            //连招数据更新
-            if (curtSkillCfg.isCombo)
-            {
-                if (comboQue.Count > 0)
-                {
-                    nextSkillID = comboQue.Dequeue();
-                }
-                else
-                {
-                    nextSkillID = 0;
-                }
-            }
-            curtSkillCfg = null;
-        }
-        SetAction(Constants.ActionDefault);
-    }
-
-    public void SetCtrl(Controller ctrl)
-    {
-        controller = ctrl;
-    }
-
-    public virtual Vector2 GetDirInput()
-    {
-        return Vector2.zero;
-    }
-
-    public void Born()
-    {
-        stateMgr.ChangeStatus(this, AniState.Born, null);
-    }
-    public void Move()
-    {
-        stateMgr.ChangeStatus(this, AniState.Move, null);
-    }
-    public void Idle()
-    {
-        stateMgr.ChangeStatus(this, AniState.Idle, null);
-    }
-    public void Attack(int skillID)
-    {
-        stateMgr.ChangeStatus(this, AniState.Attack, skillID);
-    }
-    public void Hit()
-    {
-        stateMgr.ChangeStatus(this, AniState.Hit, null);
-    }
-    public void Die()
-    {
-        stateMgr.ChangeStatus(this, AniState.Die, null);
-    }
-
-    public virtual void SetDir(Vector2 dir)
-    {
-        if (controller != null)
-        {
-            controller.Dir = dir;
-        }
-    }
-
-    public void SetActive(bool active = true)
-    {
-        if (controller != null)
-        {
-            controller.gameObject.SetActive(active);
-        }
-    }
-
-
-    public virtual Vector2 CalcTargetDir()
-    {
-        return Vector2.zero;
-    }
-
-    public virtual Vector3 GetPos()
-    {
-        return controller.transform.position;
-    }
-
-    public virtual Transform GetTrans()
-    {
-        return controller.transform;
-    }
-
-    public virtual void SkillAttack(int skillID)
-    {
-        skillMgr.SkillAttack(this, skillID);
-    }
-
-    public void RmvActionCB(int tid)
-    {
-        int index = -1;
-        for (int i = 0; i < skActionCBLst.Count; i++)
-        {
-            if (skActionCBLst[i] == tid)
-            {
-                index = i;
-                break;
-            }
-        }
-        if (index != -1)
-        {
-            skActionCBLst.RemoveAt(index);
-        }
-    }
-    public virtual void TickAILogic() { }
-
-       public virtual void SetDodge() {
+    #region 战斗信息显示
+    public virtual void SetDodge() {
         if (controller != null) {
             GameRoot.Instance.dynamicWnd.SetDodge(Name);
         }
@@ -318,16 +159,127 @@ public abstract class EntityBase
             GameRoot.Instance.dynamicWnd.SetHurt(Name, hurt);
         }
     }
-      public virtual bool GetBreakState() {
-        return true;
+    public virtual void SetHPVal(int oldval, int newval) {
+        if (controller != null) {
+            GameRoot.Instance.dynamicWnd.SetHPVal(Name, oldval, newval);
+        }
+    }
+    #endregion
+
+    public virtual void SkillAttack(int skillID) {
+        skillMgr.SkillAttack(this, skillID);
     }
 
-     public AnimationClip[] GetAniClips() {
+    public virtual Vector2 GetDirInput() {
+        return Vector2.zero;
+    }
+
+    public virtual Vector3 GetPos() {
+        return controller.transform.position;
+    }
+
+    public virtual Transform GetTrans() {
+        return controller.transform;
+    }
+
+    public AnimationClip[] GetAniClips() {
         if (controller != null) {
             return controller.ani.runtimeAnimatorController.animationClips;
         }
         return null;
     }
 
- 
+    public AudioSource GetAudio() {
+        return controller.GetComponent<AudioSource>();
+    }
+    public CharacterController GetCC() {
+        return controller.GetComponent<CharacterController>();
+    }
+
+    public virtual bool GetBreakState() {
+        return true;
+    }
+
+    public virtual Vector2 CalcTargetDir() {
+        return Vector2.zero;
+    }
+
+    public void ExitCurtSkill() {
+        canControl = true;
+
+        if (curtSkillCfg != null) {
+            if (!curtSkillCfg.isBreak) {
+                entityState = EntityState.None;
+            }
+            //连招数据更新
+            if (curtSkillCfg.isCombo) {
+                if (comboQue.Count > 0) {
+                    nextSkillID = comboQue.Dequeue();
+                }
+                else {
+                    nextSkillID = 0;
+                }
+            }
+            curtSkillCfg = null;
+        }
+        SetAction(Constants.ActionDefault);
+    }
+
+    public void RmvActionCB(int tid) {
+        int index = -1;
+        for (int i = 0; i < skActionCBLst.Count; i++) {
+            if (skActionCBLst[i] == tid) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            skActionCBLst.RemoveAt(index);
+        }
+    }
+
+    public void RmvMoveCB(int tid) {
+        int index = -1;
+        for (int i = 0; i < skMoveCBLst.Count; i++) {
+            if (skMoveCBLst[i] == tid) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            skMoveCBLst.RemoveAt(index);
+        }
+    }
+
+    public void RmvSkillCB() {
+        SetDir(Vector2.zero);
+        SetSkillMoveState(false);
+
+        for (int i = 0; i < skMoveCBLst.Count; i++) {
+            int tid = skMoveCBLst[i];
+            TimerSvc.Instance.DelTask(tid);
+        }
+
+        for (int i = 0; i < skActionCBLst.Count; i++) {
+            int tid = skActionCBLst[i];
+            TimerSvc.Instance.DelTask(tid);
+        }
+
+        //攻击被中断，删除定时回调
+        if (skEndCB != -1) {
+            TimerSvc.Instance.DelTask(skEndCB);
+            skEndCB = -1;
+        }
+        skMoveCBLst.Clear();
+        skActionCBLst.Clear();
+
+
+        //清空连招
+        if (nextSkillID != 0 || comboQue.Count > 0) {
+            nextSkillID = 0;
+            comboQue.Clear();
+            battleMgr.lastAtkTime = 0;
+            battleMgr.comboIndex = 0;
+        }
+    }
 }
--- Assets/Scripts/Battle/Manager/BattleMgr.cs
+++ Assets/Scripts/Battle/Manager/BattleMgr.cs
@@ -1,32 +1,34 @@
-﻿
+﻿/****************************************************
+	文件：BattleMgr.cs
+	作者：Plane
+	邮箱: 1785275942@qq.com
+	日期：2019/03/15 9:05   	
+	功能：战场管理器
+*****************************************************/
+
 using System;
 using System.Collections.Generic;
-using UnityEngine;
 using PEProtocol;
-public class BattleMgr : MonoBehaviour
-{
+using UnityEngine;
+
+public class BattleMgr : MonoBehaviour {
     private ResSvc resSvc;
-
     private AudioSvc audioSvc;
+
     private StateMgr stateMgr;
-
     private SkillMgr skillMgr;
+    private MapMgr mapMgr;
 
-
+    public EntityPlayer entitySelfPlayer;
     private MapCfg mapCfg;
 
-    private MapMgr mapMgr;
     private Dictionary<string, EntityMonster> monsterDic = new Dictionary<string, EntityMonster>();
-    public EntityPlayer entitySelfPlayer;
-    public bool triggerCheck = true;
 
-    public bool isPauseGame = false;
-    public void Init(int mapid, Action cb = null)
-    {
+    public void Init(int mapid, Action cb = null) {
         resSvc = ResSvc.Instance;
         audioSvc = AudioSvc.Instance;
 
-        // //初始化各管理器
+        //初始化各管理器
         stateMgr = gameObject.AddComponent<StateMgr>();
         stateMgr.Init();
         skillMgr = gameObject.AddComponent<SkillMgr>();
@@ -34,8 +36,7 @@ public class BattleMgr : MonoBehaviour
 
         //加载战场地图
         mapCfg = resSvc.GetMapCfg(mapid);
-        resSvc.AsyncLoadScene(mapCfg.sceneName, () =>
-        {
+        resSvc.AsyncLoadScene(mapCfg.sceneName, () => {
             //初始化地图数据
             GameObject map = GameObject.FindGameObjectWithTag("MapRoot");
             mapMgr = map.GetComponent<MapMgr>();
@@ -54,44 +55,75 @@ public class BattleMgr : MonoBehaviour
             ActiveCurrentBatchMonsters();
 
             audioSvc.PlayBGMusic(Constants.BGHuangYe);
-            if (cb != null)
-            {
+            if (cb != null) {
                 cb();
             }
         });
     }
 
-    public void Update()
-    {
-
-        foreach (var item in monsterDic)
-        {
+    public bool triggerCheck = true;
+    public bool isPauseGame = false;
+    public void Update() {
+        foreach (var item in monsterDic) {
             EntityMonster em = item.Value;
             em.TickAILogic();
         }
 
         //检测当前批次的怪物是否全部死亡
-        if (mapMgr != null)
-        {
-            if (triggerCheck && monsterDic.Count == 0)
-            {
+        if (mapMgr != null) {
+            if (triggerCheck && monsterDic.Count == 0) {
                 bool isExist = mapMgr.SetNextTriggerOn();
                 triggerCheck = false;
-                if (!isExist)
-                {
+                if (!isExist) {
                     //关卡结束，战斗胜利
-                    // EndBattle(true, entitySelfPlayer.HP);
+                    EndBattle(true, entitySelfPlayer.HP);
                 }
             }
         }
     }
-    public void LoadMonsterByWaveID(int wave)
-    {
-        for (int i = 0; i < mapCfg.monsterLst.Count; i++)
-        {
+
+    public void EndBattle(bool isWin, int restHP) {
+        isPauseGame = true;
+        AudioSvc.Instance.StopBGMusic();
+        BattleSys.Instance.EndBattle(isWin, restHP);
+    }
+
+    private void LoadPlayer(MapCfg mapData) {
+        GameObject player = resSvc.LoadPrefab(PathDefine.AssissnBattlePlayerPrefab);
+
+        player.transform.position = mapData.playerBornPos;
+        player.transform.localEulerAngles = mapData.playerBornRote;
+        player.transform.localScale = Vector3.one;
+
+        PlayerData pd = GameRoot.Instance.PlayerData;
+        BattleProps props = new BattleProps {
+            hp = pd.hp,
+            ad = pd.ad,
+            ap = pd.ap,
+            addef = pd.addef,
+            apdef = pd.apdef,
+            dodge = pd.dodge,
+            pierce = pd.pierce,
+            critical = pd.critical
+        };
+
+        entitySelfPlayer = new EntityPlayer {
+            battleMgr = this,
+            stateMgr = stateMgr,
+            skillMgr = skillMgr
+        };
+        entitySelfPlayer.Name = "AssassinBattle";
+        entitySelfPlayer.SetBattleProps(props);
+
+        PlayerController playerCtrl = player.GetComponent<PlayerController>();
+        playerCtrl.Init();
+        entitySelfPlayer.SetCtrl(playerCtrl);
+    }
+
+    public void LoadMonsterByWaveID(int wave) {
+        for (int i = 0; i < mapCfg.monsterLst.Count; i++) {
             MonsterData md = mapCfg.monsterLst[i];
-            if (md.mWave == wave)
-            {
+            if (md.mWave == wave) {
                 GameObject m = resSvc.LoadPrefab(md.mCfg.resPath, true);
                 m.transform.localPosition = md.mBornPos;
                 m.transform.localEulerAngles = md.mBornRote;
@@ -99,8 +131,7 @@ public class BattleMgr : MonoBehaviour
 
                 m.name = "m" + md.mWave + "_" + md.mIndex;
 
-                EntityMonster em = new EntityMonster
-                {
+                EntityMonster em = new EntityMonster {
                     battleMgr = this,
                     stateMgr = stateMgr,
                     skillMgr = skillMgr
@@ -116,68 +147,22 @@ public class BattleMgr : MonoBehaviour
 
                 m.SetActive(false);
                 monsterDic.Add(m.name, em);
-                if (md.mCfg.mType == MonsterType.Normal)
-                {
+                if (md.mCfg.mType == MonsterType.Normal) {
                     GameRoot.Instance.dynamicWnd.AddHpItemInfo(m.name, mc.hpRoot, em.HP);
                 }
-                else if (md.mCfg.mType == MonsterType.Boss)
-                {
+                else if (md.mCfg.mType == MonsterType.Boss) {
                     BattleSys.Instance.playerCtrlWnd.SetBossHPBarState(true);
                 }
             }
         }
     }
 
-
-    private void LoadPlayer(MapCfg mapData)
-    {
-        GameObject player = resSvc.LoadPrefab(PathDefine.AssissnBattlePlayerPrefab);
-
-        player.transform.position = mapData.playerBornPos;
-        player.transform.localEulerAngles = mapData.playerBornRote;
-        player.transform.localScale = Vector3.one;
-
-        PlayerData pd = GameRoot.Instance.PlayerData;
-        BattleProps props = new BattleProps
-        {
-            hp = pd.hp,
-            ad = pd.ad,
-            ap = pd.ap,
-            addef = pd.addef,
-            apdef = pd.apdef,
-            dodge = pd.dodge,
-            pierce = pd.pierce,
-            critical = pd.critical
-
-        };
-
-        entitySelfPlayer = new EntityPlayer
-        {
-            battleMgr = this,
-            stateMgr = stateMgr,
-            skillMgr = skillMgr
-        };
-        entitySelfPlayer.Name = "AssassinBattle";
-        entitySelfPlayer.SetBattleProps(props);
-
-        PlayerController playerCtrl = player.GetComponent<PlayerController>();
-        playerCtrl.Init();
-        entitySelfPlayer.SetCtrl(playerCtrl);
-
-
-    }
-
-
-    public void ActiveCurrentBatchMonsters()
-    {
-        TimerSvc.Instance.AddTimeTask((int tid) =>
-        {
-            foreach (var item in monsterDic)
-            {
+    public void ActiveCurrentBatchMonsters() {
+        TimerSvc.Instance.AddTimeTask((int tid) => {
+            foreach (var item in monsterDic) {
                 item.Value.SetActive(true);
                 item.Value.Born();
-                TimerSvc.Instance.AddTimeTask((int id) =>
-                {
+                TimerSvc.Instance.AddTimeTask((int id) => {
                     //出生1秒钟后进入Idle
                     item.Value.Idle();
                 }, 1000);
@@ -185,83 +170,43 @@ public class BattleMgr : MonoBehaviour
         }, 500);
     }
 
-
-    public void SetSelfPlayerMoveDir(Vector2 dir)
-    {
-        //设置玩家移动
-        PECommon.Log(dir.ToString());
-
-        if (entitySelfPlayer.canControl == false)
-        {
-            return;
-        }
-
-        if (entitySelfPlayer.currentAniState == AniState.Idle || entitySelfPlayer.currentAniState == AniState.Move)
-        {
-            if (dir == Vector2.zero)
-            {
-                entitySelfPlayer.Idle();
-            }
-            else
-            {
-                entitySelfPlayer.Move();
-                entitySelfPlayer.SetDir(dir);
-            }
-        }
-    }
-
-    public Vector2 GetDirInput()
-    {
-        return BattleSys.Instance.GetDirInput();
-    }
-
-    public List<EntityMonster> GetEntityMonsters()
-    {
+    public List<EntityMonster> GetEntityMonsters() {
         List<EntityMonster> monsterLst = new List<EntityMonster>();
-        foreach (var item in monsterDic)
-        {
+        foreach (var item in monsterDic) {
             monsterLst.Add(item.Value);
         }
         return monsterLst;
     }
 
-    public double lastAtkTime = 0;
-    private int[] comboArr = new int[] { 111, 112, 113, 114, 115 };
-    public int comboIndex = 0;
-    private void ReleaseNormalAtk()
-    {
-        //PECommon.Log("Click Normal Atk");
-        if (entitySelfPlayer.currentAniState == AniState.Attack)
-        {
-            //在500ms以内进行第二次点击，存数据
-            double nowAtkTime = TimerSvc.Instance.GetNowTime();
-            if (nowAtkTime - lastAtkTime < Constants.ComboSpace && lastAtkTime != 0)
-            {
-                if (comboArr[comboIndex] != comboArr[comboArr.Length - 1])
-                {
-                    comboIndex += 1;
-                    entitySelfPlayer.comboQue.Enqueue(comboArr[comboIndex]);
-                    lastAtkTime = nowAtkTime;
-                }
-                else
-                {
-                    lastAtkTime = 0;
-                    comboIndex = 0;
-                }
-            }
-        }
-        else if (entitySelfPlayer.currentAniState == AniState.Idle || entitySelfPlayer.currentAniState == AniState.Move)
-        {
-            comboIndex = 0;
-            lastAtkTime = TimerSvc.Instance.GetNowTime();
-            entitySelfPlayer.Attack(comboArr[comboIndex]);
+    public void RmvMonster(string key) {
+        EntityMonster entityMonster;
+        if (monsterDic.TryGetValue(key, out entityMonster)) {
+            monsterDic.Remove(key);
+            GameRoot.Instance.dynamicWnd.RmvHpItemInfo(key);
         }
     }
 
-    public void ReqReleaseSkill(int index)
-    {
-        switch (index)
-        {
+    #region 技能施放与角色控制
+    public void SetSelfPlayerMoveDir(Vector2 dir) {
+        //设置玩家移动
+        //PECommon.Log(dir.ToString());
+
+        if (entitySelfPlayer.canControl == false) {
+            return;
+        }
+
+        if (entitySelfPlayer.currentAniState == AniState.Idle || entitySelfPlayer.currentAniState == AniState.Move) {
+            if (dir == Vector2.zero) {
+                entitySelfPlayer.Idle();
+            }
+            else {
+                entitySelfPlayer.Move();
+                entitySelfPlayer.SetDir(dir);
+            }
+        }
+    }
+    public void ReqReleaseSkill(int index) {
+        switch (index) {
             case 0:
                 ReleaseNormalAtk();
                 break;
@@ -277,11 +222,34 @@ public class BattleMgr : MonoBehaviour
         }
     }
 
-    public bool CanRlsSkill() {
-        return entitySelfPlayer.canRlsSkill;
-    }
 
-        private void ReleaseSkill1() {
+    public double lastAtkTime = 0;
+    private int[] comboArr = new int[] { 111, 112, 113, 114, 115 };
+    public int comboIndex = 0;
+    private void ReleaseNormalAtk() {
+        //PECommon.Log("Click Normal Atk");
+        if (entitySelfPlayer.currentAniState == AniState.Attack) {
+            //在500ms以内进行第二次点击，存数据
+            double nowAtkTime = TimerSvc.Instance.GetNowTime();
+            if (nowAtkTime - lastAtkTime < Constants.ComboSpace && lastAtkTime != 0) {
+                if (comboArr[comboIndex] != comboArr[comboArr.Length - 1]) {
+                    comboIndex += 1;
+                    entitySelfPlayer.comboQue.Enqueue(comboArr[comboIndex]);
+                    lastAtkTime = nowAtkTime;
+                }
+                else {
+                    lastAtkTime = 0;
+                    comboIndex = 0;
+                }
+            }
+        }
+        else if (entitySelfPlayer.currentAniState == AniState.Idle || entitySelfPlayer.currentAniState == AniState.Move) {
+            comboIndex = 0;
+            lastAtkTime = TimerSvc.Instance.GetNowTime();
+            entitySelfPlayer.Attack(comboArr[comboIndex]);
+        }
+    }
+    private void ReleaseSkill1() {
         //PECommon.Log("Click Skill1");
         entitySelfPlayer.Attack(101);
     }
@@ -293,19 +261,13 @@ public class BattleMgr : MonoBehaviour
         //PECommon.Log("Click Skill3");
         entitySelfPlayer.Attack(103);
     }
-
-    public void RmvMonster(string key) {
-        EntityMonster entityMonster;
-        if (monsterDic.TryGetValue(key, out entityMonster)) {
-            monsterDic.Remove(key);
-            GameRoot.Instance.dynamicWnd.RmvHpItemInfo(key);
-        }
+    public Vector2 GetDirInput() {
+        return BattleSys.Instance.GetDirInput();
     }
 
-       public void EndBattle(bool isWin, int restHP) {
-        isPauseGame = true;
-        AudioSvc.Instance.StopBGMusic();
-        BattleSys.Instance.EndBattle(isWin, restHP);
+    public bool CanRlsSkill() {
+        return entitySelfPlayer.canRlsSkill;
     }
 
+    #endregion
 }
